0681eb1e7e647fc6979e585ed74e68cc5f968e75
rpoddighe <poddighe.renzo@gmail.com>
2014-12-08 12:06:21 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pybursts
+
+## Description
+This is a Python port of the [R implementation](http://cran.r-project.org/web/packages/bursts/index.html) of Kleinberg's algorithm (described in ['Bursty and Hierarchical Structure in Streams'](http://www.cs.cornell.edu/home/kleinber/bhs.pdf)). A bigger readme will soon follow.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'pybursts',
+	packages = ['pybursts'], # this must be the same as the name above
+	version = '0.1',
+	description = 'A Python port from the \'burst detection\' algorithm by Kleinberg, originally implemented in R',
+	author = 'Renzo Poddighe',
+	author_email = 'poddighe.renzo@gmail.com',
+	url = 'https://github.com/rpoddighe/pybursts', # use the URL to the github repo
+	download_url = 'https://github.com/rpoddighe/pybursts/tarball/0.1', # I'll explain this in a second
+	keywords = ['burst detection', 'data mining', 'text mining'], # arbitrary keywords
+	classifiers = [],
+)
